7e3290481174e2a333b03f6f98c7177385fa7f76
ethan2lee <714657067@qq.com>
2018-06-19 14:30:19 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+ 
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+app.use(express.static('public'));
+ 
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+ 
+app.post('/process_post', urlencodedParser, function (req, res) {
+ 
+   // 输出 JSON 格式
+   var response = {
+       "姓名":req.body.personal_name,
+       "毕业后地点":req.body.address_graduation_off,
+       "公司名称或高校名称":req.body.name_of_company_or_school,
+       "联系电话":req.body.phone_number,
+       "常用邮箱":req.body.email,
+       "微信":req.body.weichat
+       
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
